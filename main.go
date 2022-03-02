76d51ae60b168bf2870376082b4c5231df46b5e1
Robson Garcia Formoso <robson.formoso@mercadolivre.com>
2022-03-02 18:11:04 -0300

    Adds main

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/formoso/furl/furl"
+)
+
+func main() {
+	if len(os.Args) < 1 {
+		fmt.Fprintf(os.Stderr, "URL parameter not informed\n")
+		os.Exit(1)
+	}
+	url := os.Args[1]
+	resp, err := furl.Get(url)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error fetching URL:%v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("%dms %7d %s %s\n", resp.ElapsedTime, resp.NBytes, resp.Url, resp.Body)
+
+}
